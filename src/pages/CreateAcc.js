093dd9ab75fe528df1ea86e8e52ddbc76661c7cb
adarsh3699 <adarsh3699@gmail.com>
2022-04-30 23:08:59 +0530

    create acc done

--- src/pages/CreateAcc.js
+++ src/pages/CreateAcc.js
@@ -1,35 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { apiCall } from "../utils";
+import "../css/login.css";
 
-function createAcc() {
+function CreateAcc() {
+
+    const [userName, setuserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [confPassword, setConfPassword] = useState("");
+    const [msg, setMsg] = useState("");
+
+    function handelNewUsername(e) {
+        setuserName(e.target.value)
+    }
+
+    function handelNewPassword(e) {
+        setPassword(e.target.value)
+    }
+
+    function handelConfirmPassword(e) {
+        setConfPassword(e.target.value)
+    }
+
+    async function handleFormSubmit(e) {
+        e.preventDefault();
+
+        if(userName !== "" && password !== "" && confPassword !== "") {
+            if(password === confPassword) {
+                const apiResp = await apiCall("http://localhost:4000/api/users", false , "post", { username: userName , password: password});
+                console.log(apiResp);
+            } else {
+                setMsg("Passwords didn't match. Try again.")
+            }
+        } else {
+            setMsg("Please enter all data.")
+        }
+    }
+
     return (
-        <>
-            <div id="background">
-                <div id="wrapper">
-                    <div id="form">
-                        <div>
-                            <input type="text" placeholder="User Name" id="newUserName" />
-                        </div>
-
-                        <div>
-                            <input type="Password" placeholder="Password" id="newPassword" />
-                        </div>
-
-                        <div>
-                            <input type="Password" placeholder="Confirm Password" id="confirmPass" />
-                        </div>
-
-                        <hr />
-
-                        <div id="updateMsg" className="red"></div>
-
-                        <div>
-                            <button id="signup">Sign Up</button>
-                        </div>
+        <div id="background">
+            <div id="wrapper">
+                <form id="form" onSubmit={ handleFormSubmit }>
+                    <div>
+                        <input type="text" placeholder="User Name" id="newUserName" value={ userName } onChange={handelNewUsername} />
                     </div>
-                </div>
+
+                    <div>
+                        <input type="Password" placeholder="Password" id="newPassword" value={ password } onChange={handelNewPassword} />
+                    </div>
+
+                    <div>
+                        <input type="Password" placeholder="Confirm Password" id="confirmPass" value={ confPassword } onChange={handelConfirmPassword} />
+                    </div>
+
+                    <hr />
+
+                    <div id="updateMsg" className="red"> {msg} </div>
+
+                    <div>
+                        <button id="signup">Sign Up</button>
+                    </div>
+                </form>
             </div>
-        </>
+        </div>
     )
 }
 
-export default createAcc;
+export default CreateAcc;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Cookies from 'universal-cookie';
+import { apiCall } from "../utils";
+import "../css/home.css";
+import addIcon from "../img/add.png"
 
-function HomePage() {
+function HomePage() {  
+    const cookies = new Cookies();
+    const myUserId = cookies.get('userId');
+    const [isActive, setActive] = useState(false);
+    const [msg, setMsg] = useState("");
+
+    function handleAddBtnClick(e) {
+        setActive(!isActive)
+    }
+
+     useEffect(function() {
+        return async function () {
+            if (myUserId){
+                const apiResp =  await apiCall("http://localhost:4000/api/notes?userId="+ myUserId);
+                if (apiResp.statusCode === 200) {
+                    for (let i = 0; i < apiResp.data.length; i++) {
+                        console.log(apiResp.data[i]);
+                    }
+                    
+                } else {
+                    setMsg(apiResp.msg)
+                }
+
+            }
+        }
+    }, [isActive]);
+
     return (
         <>
-            <div>HomePage</div>
+            <div id="bar">
+                <input type="text" id="inputBox" autoFocus placeholder="Take a note..." />
+            </div>
+            <div id="addButton" onClick={ handleAddBtnClick } >
+                <img src={ addIcon } height="30px" id="addImg" />
+                    <div id="option" className={isActive ? 'showOption': null}  >
+                        <div id="addNotes" >Note</div>
+                        <div id="addTodos" >ToDos</div>
+                    </div>
+            </div>
+
+            <div id="background">
+                <div id="msg">{ msg }</div>
+                <div id="list"> </div>
+            </div>
         </>
     )
 }
