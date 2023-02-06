04703458a97fa830b7c06717d01981c29f63f5f3
adarsh3699 <adarsh3699@gmail.com>
2023-02-06 16:26:46 +0530

    about page

--- src/components/settingsPage/aboutSettings/AboutSettings.js
+++ src/components/settingsPage/aboutSettings/AboutSettings.js
@@ -3,7 +3,16 @@ import React from 'react';
 import './aboutSettings.css';
 
 function AboutSettings() {
-    return <div className="aboutSettings">This tab will be available soon. Thank you for your patience!</div>;
+    return (
+        <div className="aboutSettings">
+            <div className="aboutSettingsTitle">What's New :-</div>
+            <li>Our new version 2.1.0 has been released.</li>
+            <li>Introduced our new UI for a better user experience.</li>
+            <li>Now we have migrated to Firebase for fast and secure database connections.</li>
+            <li>You can now store both to-do and note types in the same file.</li>
+            <li>Optimize for fast and smooth performance.</li>
+        </div>
+    );
 }
 
 export default AboutSettings;
